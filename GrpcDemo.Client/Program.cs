7f17d156f2de085613a1cd8ec8c4f65f89cbe0bc
eric.p <eric.p>
2022-06-01 23:03:45 +0800

    feat: dotnet 6 startup

--- GrpcDemo.Client/Program.cs
+++ GrpcDemo.Client/Program.cs
@@ -1,28 +1,55 @@
+using System.Reflection;
+using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using GrpcDemo.Client.Applibs;
+using GrpcDemo.Client.Model;
+using GrpcDemo.Client.Worker;
+using GrpcDemo.Grpc.Service;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
 using NLog.Web;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
-namespace GrpcDemo.Client
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.SetMinimumLevel(LogLevel.Trace);
+builder.Host.UseNLog();
+LogManager.Configuration = new NLogLoggingConfiguration(ConfigHelper.Config.GetSection("NLog"));
+
+builder.WebHost.UseUrls($"http://*:{8086}");
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    var asm = Assembly.GetExecutingAssembly();
 
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            return Host.CreateDefaultBuilder(args)
-                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
-                .ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    logging.SetMinimumLevel(LogLevel.Trace);
-                }).UseNLog();
-        }
-    }
-}
+    // 指定處理client指令的handler
+    builder.RegisterAssemblyTypes(asm)
+        .Where(t => t.IsAssignableTo<IActionHandler>())
+        .Named<IActionHandler>(t => t.Name.Replace("Handler", string.Empty).ToLower())
+        .PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies)
+        .SingleInstance();
+});
+
+builder.Services.AddHostedService<BidirectionalService>();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+app.Run();
--- GrpcDemo.Server/Program.cs
+++ GrpcDemo.Server/Program.cs
@@ -1,36 +1,63 @@
+using System;
 using System.Net;
+using Autofac;
 using Autofac.Core;
 using Autofac.Extensions.DependencyInjection;
+using GrpcDemo.Server;
+using GrpcDemo.Server.Applibs;
+using GrpcDemo.Server.Jobs;
+using GrpcDemo.Server.Model;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
 using NLog.Web;
 using Quartz;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
-namespace GrpcDemo.Server
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.SetMinimumLevel(LogLevel.Trace);
+builder.Host.UseNLog();
+LogManager.Configuration = new NLogLoggingConfiguration(ConfigHelper.Config.GetSection("NLog"));
+
+builder.Services.AddGrpc();
+builder.WebHost.UseUrls($"http://*:{8085}");
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+builder.Services.AddQuartz(q =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    q.UseMicrosoftDependencyInjectionJobFactory();
+    var pingJobKey = new JobKey(nameof(SendPingActionJob));
+    q.AddJob<SendPingActionJob>(opt =>
+        opt.WithIdentity(pingJobKey)
+    );
+    q.AddTrigger(opt => opt
+        .ForJob(pingJobKey)
+        .WithIdentity($"{nameof(SendPingActionJob)}-Trigger")
+        .WithCronSchedule("*/5 * * * * ?")
+    );
+});
 
-        // Additional configuration is required to successfully run gRPC on macOS.
-        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            return Host.CreateDefaultBuilder(args)
-                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.ConfigureKestrel(op => { op.Listen(IPAddress.Any, 8085); });
-                    webBuilder.UseStartup<Startup>();
-                }).ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    logging.SetMinimumLevel(LogLevel.Trace);
-                }).UseNLog();
-        }
-    }
-}
+builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+
+builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+{
+    builder.RegisterType<ClientCollection>()
+        .PropertiesAutowired(PropertyWiringOptions.AllowCircularDependencies)
+        .SingleInstance();
+});
+
+var app = builder.Build();
+
+app.MapGrpcService<GreeterService>();
+app.MapGrpcService<BidirectionalService>();
+
+LogManager.GetCurrentClassLogger().Info($"Process Count:{Environment.ProcessorCount}");
+
+app.Run();
